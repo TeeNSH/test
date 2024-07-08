9b9c0ff034768eb6643df389cbe5fba3995caca6
TeeNSH <156264415+TeeNSH@users.noreply.github.com>
2024-07-08 21:00:22 +0300

    Update Luga.ps1

--- Luga.ps1
+++ Luga.ps1
@@ -1,6 +1,6 @@
 # Включаем поддержку кириллицы в выводе
-$OutputEncoding = [Console]::OutputEncoding = [Text.Encoding]::UTF8
-
+# $OutputEncoding = [Console]::OutputEncoding = [Text.Encoding]::UTF8
+[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
 # Хэш-таблица с именами и адресами устройств
 $devices = @{
     "Kassa 6" = "192.168.104.101"
